1c30ef370d82d409843ba9a170e01ced5de616a6
Julien Lattari <33458171+Jaradard@users.noreply.github.com>
2025-04-15 09:53:41 +0200

    Create user_profile.ps1

---
+++ user_profile.ps1
@@ -0,0 +1,35 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Prompt
+Import-Module posh-git
+
+# oh-my-posh theme
+oh-my-posh init pwsh --config 'C:\Users\admcol.lattari\Documents\oh-my-posh\themes\jaradard.omp.json' | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons 
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PSFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
